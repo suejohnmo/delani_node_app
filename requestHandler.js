7d80d177a9d66cad0f8beffc12dba554751ef9f7
suejohnmo <susan.mwaura@gmail.com>
2020-07-20 13:13:06 +0300

    adding files to project

--- requestHandler.js
+++ requestHandler.js
@@ -1,27 +1,27 @@
-/let exec = require("child_process").exec;
+let fs = require("fs");
 
 // function for handling the business logic for index.html
-function index (response){
-   console.log("Request handler for index was called.")
-  
-   exec("ls -lah", function(error, stdout, stderr){
-      
-       response.writeHead(200, {"Content-type": "text/plain"});
-       response.write(stdout);
-       response.end(); 
-   })
+function index(response) {
+  console.log("Request handler for index was called.");
 
+  // Reading the index.html file and attaching the content to the response
+  fs.readFile("./public/index.html", function (error, data) {
+    if (error) {
+      console.log(err);
+    }
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.write(data);
+    response.end();
+  });
 }
 
 // function for handling the business logic for  portfolio.html
-function portfolio(response){
-   console.log("Request for handler for portfolio was called.")
-   response.writeHead(200, {"Content-type": "text/plain"});
-   response.write("These are some of our portfolio projects");
-   response.end();
-  
+function portfolio(response) {
+  console.log("Request for handler for portfolio was called.");
+  response.writeHead(200, { "Content-type": "text/plain" });
+  response.write("These are some of our portfolio projects");
+  response.end();
 }
-
 
 exports.index = index;
 exports.portfolio = portfolio;
